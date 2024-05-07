7bd959480ee5251e78f0799d884911a258b8b622
Usman Ali <usmanali9525@gmail.com>
2024-05-08 01:45:22 +0500

    Update

--- DiseaseDetection.py
+++ DiseaseDetection.py
@@ -5,12 +5,11 @@ from PIL import Image
 
 app = Flask(__name__)
 
-# Load the trained model
-model_path = 'NewReducedPlantDiseaseDetection1.h5'
+
+model_path = 'ReducedPlantDiseaseDetection.h5'
 loaded_model = tf.keras.models.load_model(model_path)
 print("Model loaded successfully.")
 
-# Class names
 class_names = [
     'Corn__common_rust', 'Corn__gray_leaf_spot', 'Corn__healthy', 'Corn__northern_leaf_blight', 
     'Rice__brown_spot', 'Rice__healthy', 'Rice__leaf_blast', 'Tea__algal_leaf', 'Tea__brown_blight', 
@@ -18,25 +17,21 @@ class_names = [
     'Tomato__healthy', 'Tomato__leaf_mold', 'Tomato__septoria_leaf_spot', 'Tomato__target_spot'
 ]
 
-# Function to preprocess the image
 def preprocess_image(file):
     img = Image.open(file)
     img = img.resize((224, 224))
     img_array = np.array(img)
     
-    # Check number of channels
     if img_array.shape[-1] == 4:
-        # Convert RGBA to RGB
         img_array = img_array[:, :, :3]
     
     img_array = np.expand_dims(img_array, axis=0)
     return img_array
 
-# Function to get predictions
 def get_predictions(img_array):
     predictions = loaded_model.predict(img_array)
     predicted_class = np.argmax(predictions)
-    return class_names[predicted_class]  # Get class name from class_names list
+    return class_names[predicted_class]  
 
 @app.route('/')
 def home():
@@ -59,7 +54,7 @@ def predict():
             predict_disease = get_predictions(img_array).split('__')[1]
             return render_template('index.html', predicted_plant=predicted_plant, predict_disease=predict_disease)
         except Exception as e:
-            return jsonify({'error': str(e)})  # Handle exception
+            return jsonify({'error': str(e)}) 
 
 if __name__ == '__main__':
     app.run(debug=True)
